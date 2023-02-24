b1b161c8cf8ad42b89675f30cbb9c3835ab482de
alexandre.milani <alexandre.milani00@gmail.com>
2023-02-23 21:39:34 -0400

    New Bearing script.

--- Parts/Bearing.py
+++ Parts/Bearing.py
@@ -4,193 +4,236 @@ import Part
 
 
 class Bearing:
-    _instance = None
+    ActiveDocument = FreeCAD.ActiveDocument
+    ActiveView = FreeCADGui.ActiveDocument.ActiveView
 
-    document = None
-    view = None
+    def __init__(self):
+        self.Part001 = self.create(self.Part001)
+        self.Body001 = self.create(self.Body001)
+        self.Sketch001Body001 = self.create(self.Sketch001Body001)
+        self.Pad001Body001 = self.create(self.Pad001Body001)
+        self.Body002 = self.create(self.Body002)
+        self.Sketch001Body002 = self.create(self.Sketch001Body002)
+        self.Pad001Body002 = self.create(self.Pad001Body002)
+        self.Body003 = self.create(self.Body003)
+        self.Sketch001Body003 = self.create(self.Sketch001Body003)
+        self.Pad001Body003 = self.create(self.Pad001Body003)
 
-    part001 = None
-    body001 = None
-    sketch001body001 = None
-    revolution001body001 = None
-    body002 = None
-    sketch001body002 = None
-    pad001body002 = None
-    body003 = None
-    sketch001body003 = None
-    pad001body003 = None
-    pad002body003 = None
-    sketch002body003 = None
-    pocket001body003 = None
-    sketch003body003 = None
-    pocket002body003 = None
+        self.Body002.ViewObject.ShapeColor = (0.2, 0.2, 0.2, 0.0)
 
-    def __new__(self):
-        if self._instance is None:
-            self._instance = super(__class__, self).__new__(self)
-        return self._instance
+    def create(self, method):
+        property = method(self.__class__.__name__ + method.__name__)
+        self.ActiveDocument.recompute()
+        return(property)
 
-    def create(self, document=None):
-        self.document = FreeCAD.getDocument(document) if document else FreeCAD.ActiveDocument
-        self.view = FreeCADGui.getDocument(document).ActiveView if document else FreeCADGui.ActiveDocument.ActiveView
+    def Part001(self, name):
+        if self.ActiveDocument.getObject(name):
+            self.ActiveDocument.getObject(name).removeObjectsFromDocument()
+            self.ActiveDocument.removeObject(name)
 
-        self.__part001('Part001Bearing')
-        self.__body001('Body001Bearing')
-        self.__sketch001body001('Sketch001Body001Bearing')
-        self.__revolution001body001('Revolution001Body001Bearing')
-        self.__body002('Body002Bearing')
-        self.__sketch001body002('Sketch001Body002Bearing')
-        self.__pad001body002('Pad001Body002Bearing')
-        self.__body003('body003Bearing')
-        self.__sketch001body003('Sketch001body003Bearing')
-        self.__pad001body003('Pad001body003Bearing')
-        self.__pad002body003('Pad002body003Bearing')
-        self.__sketch002body003('Sketch002body003Bearing')
-        self.__pocket001body003('Pocket001body003Bearing')
-        self.__sketch003body003('Sketch003body003Bearing')
-        self.__pocket002body003('Pocket002body003Bearing')
+        Part001 = self.ActiveDocument.addObject('App::Part', name)
+        Part001.Origin = self.ActiveDocument.addObject('App::Origin', name + 'Origin')
+        self.ActiveDocument.removeObject('Origin')
+        return(Part001)
 
-        self.view.setActiveObject('pdbody', None)
-        self.view.setActiveObject('part', None)
+    def Body001(self, name):
+        Body001 = self.Part001.newObject('PartDesign::Body', name)
+        Body001.Origin = self.ActiveDocument.addObject('App::Origin', name + 'Origin')
+        self.ActiveDocument.removeObject('Origin')
+        return(Body001)
 
-        self.body002.ViewObject.ShapeColor = (0.2, 0.2, 0.2, 0.0)
-
-    def __part001(self, name):
-        if self.document.getObject(name):
-            self.document.getObject(name).removeObjectsFromDocument()
-            self.document.removeObject(name)
-            self.document.recompute()
-
-        self.part001 = self.document.addObject('App::Part', name)
-        self.view.setActiveObject('part', self.part001)
-        self.document.recompute()
-
-    def __body001(self, name):
-        self.body001 = self.part001.newObject('PartDesign::Body', name)
-        self.view.setActiveObject('pdbody', self.body001)
-        self.document.recompute()
-
-    def __sketch001body001(self, name):
-        self.sketch001body001 = self.body001.newObject('Sketcher::SketchObject', name)
-        self.sketch001body001.Support = self.body001.Origin.OriginFeatures[4]  # XZ_Plane
-        self.sketch001body001.MapMode = 'FlatFace'
-        self.sketch001body001.Visibility = False
-        self.document.recompute()
-
-        self.sketch001body001.addGeometry([
-            Part.ArcOfParabola(Part.Parabola(
-                FreeCAD.Vector(22/4, 0, 0),
-                FreeCAD.Vector(22/2, 0, 0),
-                FreeCAD.Vector(0, 0, 1)), -7/2, 7/2
-            ),  # 0
-            Part.LineSegment(FreeCAD.Vector(19/2, 7/2, 0), FreeCAD.Vector(19/2, -7/2, 0)),  # 1
-        ], False)
-        self.sketch001body001.addGeometry([
-            Part.LineSegment(FreeCAD.Vector(
-                self.sketch001body001.Geometry[0].StartPoint.x,
-                self.sketch001body001.Geometry[0].StartPoint.y, 0),
-                FreeCAD.Vector(19/2, 7/2, 0)
-            ),  # 3
-            Part.LineSegment(FreeCAD.Vector(
-                self.sketch001body001.Geometry[0].EndPoint.x,
-                self.sketch001body001.Geometry[0].EndPoint.y, 0),
-                FreeCAD.Vector(19/2, -7/2, 0)
-            ),  # 4
-        ], False)
-        self.document.recompute()
-
-    def __revolution001body001(self, name):
-        self.revolution001body001 = self.body001.newObject('PartDesign::Revolution', name)
-        self.revolution001body001.Profile = self.sketch001body001
-        self.revolution001body001.ReferenceAxis = (self.sketch001body001, ['V_Axis'])
-        self.document.recompute()
-
-    def __body002(self, name):
-        self.body002 = self.part001.newObject('PartDesign::Body', name)
-        self.view.setActiveObject('pdbody', self.body002)
-        self.document.recompute()
-
-    def __sketch001body002(self, name):
-        self.sketch001body002 = self.body002.newObject('Sketcher::SketchObject', name)
-        self.sketch001body002.Support = self.body002.Origin.OriginFeatures[3]  # XY_Plane
-        self.sketch001body002.MapMode = 'FlatFace'
-        self.sketch001body002.Visibility = False
-        self.document.recompute()
-
-        self.sketch001body002.addGeometry([
-            Part.Circle(FreeCAD.Vector(0, 0, 0), FreeCAD.Vector(0, 0, 1), 12/2),
-            Part.Circle(FreeCAD.Vector(0, 0, 0), FreeCAD.Vector(0, 0, 1), 19/2),
-        ], False)
-        self.document.recompute()
-
-    def __pad001body002(self, name):
-        self.pad001body002 = self.body002.newObject('PartDesign::Pad', name)
-        self.pad001body002.Profile = self.sketch001body002
-        self.pad001body002.Midplane = True
-        self.pad001body002.Length = 6.8
-        self.document.recompute()
-
-    def __body003(self, name):
-        self.body003 = self.part001.newObject('PartDesign::Body', name)
-        self.view.setActiveObject('pdbody', self.body003)
-        self.document.recompute()
-
-    def __sketch001body003(self, name):
-        self.sketch001body003 = self.body003.newObject('Sketcher::SketchObject', name)
-        self.sketch001body003.Support = self.body003.Origin.OriginFeatures[3]  # XY_Plane
-        self.sketch001body003.MapMode = 'FlatFace'
-        self.sketch001body003.Visibility = False
-        self.document.recompute()
-
-        self.sketch001body003.addGeometry([
+    def Sketch001Body001(self, name):
+        Sketch001Body001 = self.Body001.newObject('Sketcher::SketchObject', name)
+        Sketch001Body001.Support = self.Body001.Origin.OriginFeatures[3] # XY_Plane
+        Sketch001Body001.MapMode = 'FlatFace'
+        Sketch001Body001.addGeometry([
             Part.Circle(FreeCAD.Vector(0, 0, 0), FreeCAD.Vector(0, 0, 1), 8/2),
             Part.Circle(FreeCAD.Vector(0, 0, 0), FreeCAD.Vector(0, 0, 1), 12/2),
         ], False)
-        self.document.recompute()
+        Sketch001Body001.Visibility = False
+        return(Sketch001Body001)
 
-    def __pad001body003(self, name):
-        self.pad001body003 = self.body003.newObject('PartDesign::Pad', name)
-        self.pad001body003.Profile = self.sketch001body003
-        self.pad001body003.Midplane = True
-        self.pad001body003.Length = 7
-        self.document.recompute()
+    def Pad001Body001(self, name):
+        Pad001Body001 = self.Body001.newObject('PartDesign::Pad', name)
+        Pad001Body001.Profile = self.Sketch001Body001
+        Pad001Body001.Midplane = True
+        Pad001Body001.Length = 7
+        return(Pad001Body001)
 
-    def __pad002body003(self, name):
-        self.pad002body003 = self.body003.newObject('PartDesign::Pad', name)
-        self.pad002body003.Profile = (self.pad001body003, ['Face4'])
-        self.pad002body003.Length = 4
-        self.document.recompute()
+    def Body002(self, name):
+        Body002 = self.Part001.newObject('PartDesign::Body', name)
+        Body002.Origin = self.ActiveDocument.addObject('App::Origin', name + 'Origin')
+        self.ActiveDocument.removeObject('Origin')
+        return(Body002)
 
-    def __sketch002body003(self, name):
-        self.sketch002body003 = self.body003.newObject('Sketcher::SketchObject', name)
-        self.sketch002body003.Support = self.body003.Origin.OriginFeatures[5]  # YZ_Plane
-        self.sketch002body003.MapMode = 'FlatFace'
-        self.sketch002body003.Visibility = False
-        self.document.recompute()
+    def Sketch001Body002(self, name):
+        Sketch001Body002 = self.Body002.newObject('Sketcher::SketchObject', name)
+        Sketch001Body002.Support = self.Body002.Origin.OriginFeatures[3] # XY_Plane
+        Sketch001Body002.MapMode = 'FlatFace'
+        Sketch001Body002.addGeometry([
+            Part.Circle(FreeCAD.Vector(0, 0, 0), FreeCAD.Vector(0, 0, 1), 12/2),
+            Part.Circle(FreeCAD.Vector(0, 0, 0), FreeCAD.Vector(0, 0, 1), 19/2),
+        ], False)
+        Sketch001Body002.Visibility = False
+        return(Sketch001Body002)
 
-        self.sketch002body003.addGeometry([
+    def Pad001Body002(self, name):
+        Pad001Body002 = self.Body002.newObject('PartDesign::Pad', name)
+        Pad001Body002.Profile = self.Sketch001Body002
+        Pad001Body002.Midplane = True
+        Pad001Body002.Length = 6.8
+        return(Pad001Body002)
+
+    def Body003(self, name):
+        Body003 = self.Part001.newObject('PartDesign::Body', name)
+        Body003.Origin = self.ActiveDocument.addObject('App::Origin', name + 'Origin')
+        self.ActiveDocument.removeObject('Origin')
+        self.ActiveDocument.recompute()
+        return(Body003)
+
+    def Sketch001Body003(self, name):
+        Sketch001Body003 = self.Body003.newObject('Sketcher::SketchObject', name)
+        Sketch001Body003.Support = self.Body003.Origin.OriginFeatures[3] # XY_Plane
+        Sketch001Body003.MapMode = 'FlatFace'
+        Sketch001Body003.addGeometry([
+            Part.Circle(FreeCAD.Vector(0, 0, 0), FreeCAD.Vector(0, 0, 1), 19/2),
+            Part.Circle(FreeCAD.Vector(0, 0, 0), FreeCAD.Vector(0, 0, 1), 22/2),
+        ], False)
+        Sketch001Body003.Visibility = False
+        return(Sketch001Body003)
+
+    def Pad001Body003(self, name):
+        Pad001Body003 = self.Body003.newObject('PartDesign::Pad', name)
+        Pad001Body003.Profile = self.Sketch001Body003
+        Pad001Body003.Midplane = True
+        Pad001Body003.Length = 7
+        return(Pad001Body003)
+
+
+class BearingKFL8(Bearing):
+    def __init__(self):
+        self.Part001 = self.create(self.Part001)
+        self.Body001 = self.create(self.Body001)
+        self.Sketch001Body001 = self.create(self.Sketch001Body001)
+        self.Pad001Body001 = self.create(self.Pad001Body001)
+        self.Pad002Body001 = self.create(self.Pad002Body001)
+        self.Sketch002Body001 = self.create(self.Sketch002Body001)
+        self.Pocket001Body001 = self.create(self.Pocket001Body001)
+        self.Sketch003Body001 = self.create(self.Sketch003Body001)
+        self.Pocket002Body001 = self.create(self.Pocket002Body001)
+        self.Body002 = self.create(self.Body002)
+        self.Sketch001Body002 = self.create(self.Sketch001Body002)
+        self.Pad001Body002 = self.create(self.Pad001Body002)
+        self.Body003 = self.create(self.Body003)
+        self.Sketch001Body003 = self.create(self.Sketch001Body003)
+        self.Revolution001Body003 = self.create(self.Revolution001Body003)
+
+        self.Body002.ViewObject.ShapeColor = (0.2, 0.2, 0.2, 0.0)
+
+    def Part001(self, name):
+        return(super().Part001(name))
+
+    def Body001(self, name):
+        return(super().Body001(name))
+
+    def Sketch001Body001(self, name):
+        return(super().Sketch001Body001(name))
+
+    def Pad001Body001(self, name):
+        return(super().Pad001Body001(name))
+
+    def Pad002Body001(self, name):
+        Pad002Body001 = self.Body001.newObject('PartDesign::Pad', name)
+        Pad002Body001.Profile = (self.Pad001Body001, ['Face4'])
+        Pad002Body001.Length = 4
+        return(Pad002Body001)
+
+    def Sketch002Body001(self, name):
+        Sketch002Body001 = self.Body001.newObject('Sketcher::SketchObject', name)
+        Sketch002Body001.Support = self.Body001.Origin.OriginFeatures[4] # XZ_Plane
+        Sketch002Body001.MapMode = 'FlatFace'
+        Sketch002Body001.addGeometry([
             Part.Circle(FreeCAD.Vector(0, 7/2+4/2, 0), FreeCAD.Vector(0, 0, 1), 3/2),
         ], False)
-        self.document.recompute()
+        Sketch002Body001.Visibility = False
+        return(Sketch002Body001)
 
-    def __pocket001body003(self, name):
-        self.pocket001body003 = self.body003.newObject('PartDesign::Pocket', name)
-        self.pocket001body003.Profile = self.sketch002body003
-        self.pocket001body003.Reversed = True
-        self.pocket001body003.Type = 'ThroughAll'
-        self.document.recompute()
+    def Pocket001Body001(self, name):
+        Pocket001Body001 = self.Body001.newObject('PartDesign::Pocket', name)
+        Pocket001Body001.Profile = self.Sketch002Body001
+        Pocket001Body001.Reversed = True
+        Pocket001Body001.Type = 'ThroughAll'
+        return(Pocket001Body001)
 
-    def __sketch003body003(self, name):
-        self.sketch003body003 = self.body003.newObject('Sketcher::SketchObject', name)
-        self.sketch003body003.Support = self.body003.Origin.OriginFeatures[4]  # XZ_Plane
-        self.sketch003body003.MapMode = 'FlatFace'
-        self.sketch003body003.Geometry = self.sketch002body003.Geometry
-        self.sketch003body003.Visibility = False
-        self.document.recompute()
+    def Sketch003Body001(self, name):
+        Sketch003Body001 = self.Body001.newObject('Sketcher::SketchObject', name)
+        Sketch003Body001.Support = self.Sketch002Body001.Support
+        Sketch003Body001.MapMode = self.Sketch002Body001.MapMode
+        Sketch003Body001.AttachmentOffset = FreeCAD.Placement(
+            FreeCAD.Vector(0, 0, 0), FreeCAD.Rotation(App.Vector(0, 1, 0), 80)
+        )
+        Sketch003Body001.Geometry = self.Sketch002Body001.Geometry
+        Sketch003Body001.Visibility = self.Sketch002Body001.Visibility
+        return(Sketch003Body001)
 
-    def __pocket002body003(self, name):
-        self.pocket002body003 = self.body003.newObject('PartDesign::Pocket', name)
-        self.pocket002body003.Profile = self.sketch003body003
-        self.pocket002body003.Reversed = True
-        self.pocket002body003.Type = 'ThroughAll'
-        self.document.recompute()
+    def Pocket002Body001(self, name):
+        Pocket002Body001 = self.Body001.newObject('PartDesign::Pocket', name)
+        Pocket002Body001.Profile = self.Sketch003Body001
+        Pocket002Body001.Reversed = True
+        Pocket002Body001.Type = 'ThroughAll'
+        return(Pocket002Body001)
+
+    def Body002(self, name):
+        return(super().Body002(name))
+
+    def Sketch001Body002(self, name):
+        return(super().Sketch001Body002(name))
+
+    def Pad001Body002(self, name):
+        return(super().Pad001Body002(name))
+
+    def Body003(self, name):
+        return(super().Body003(name))
+
+    def Sketch001Body003(self, name):
+        Sketch001 = self.Body003.newObject('Sketcher::SketchObject', name)
+        Sketch001.Support = self.Body003.Origin.OriginFeatures[4] # XZ_Plane
+        Sketch001.MapMode = 'FlatFace'
+        Sketch001.Visibility = False
+        Sketch001.addGeometry([
+            Part.ArcOfParabola(Part.Parabola(
+                    FreeCAD.Vector(22/4, 0, 0),
+                    FreeCAD.Vector(22/2, 0, 0),
+                    FreeCAD.Vector(0, 0, 1),
+                ), -7/2, 7/2), # 0
+            Part.LineSegment(
+                FreeCAD.Vector(19/2, 7/2, 0),
+                FreeCAD.Vector(19/2, -7/2, 0)
+            ), # 1
+        ], False)
+        Sketch001.addGeometry([
+            Part.LineSegment(FreeCAD.Vector(
+                Sketch001.Geometry[0].StartPoint.x,
+                Sketch001.Geometry[0].StartPoint.y, 0),
+                FreeCAD.Vector(19/2, 7/2, 0)
+            ), # 3
+            Part.LineSegment(FreeCAD.Vector(
+                Sketch001.Geometry[0].EndPoint.x,
+                Sketch001.Geometry[0].EndPoint.y, 0),
+                FreeCAD.Vector(19/2, -7/2, 0)
+            ), # 4
+        ], False)
+        self.ActiveDocument.recompute()
+        return(Sketch001)
+
+    def Revolution001Body003(self, name):
+        Revolution001 = self.Body003.newObject('PartDesign::Revolution', name)
+        Revolution001.ReferenceAxis = (self.Sketch001Body003, ['V_Axis'])
+        Revolution001.Profile = self.Sketch001Body003
+        self.ActiveDocument.recompute()
+        return(Revolution001)
+
+
+if __name__ == '__main__':
+    # Bearing()
+    BearingKFL8()
